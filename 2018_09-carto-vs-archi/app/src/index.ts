946ce66f43aea74cd90a5c30f898aa242152251e
Mariana Danielova <mariana.danielova@eli-beams.eu>
2018-09-19 22:56:58 +0200

    carto vs archi app load data

--- 2018_09-carto-vs-archi/app/src/index.ts
+++ 2018_09-carto-vs-archi/app/src/index.ts
@@ -1,28 +1,62 @@
-import * as THREE from "three"
+import * as THREE from "three";
+import * as Papa from "papaparse";
 
-// create scene
-var scene = new THREE.Scene();
-scene.background = new THREE.Color( 0xffffff );
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+parseData("data/route_coordinates.csv", createVisualization);
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+function parseData(url: string, callBack: any) {
+    Papa.parse(url, {
+        download: true,
+        dynamicTyping: true,
+        delimiter: ",",
+        fastMode: true,
+        header: true,
+        complete: function (results) {
+            callBack(results.data);
+        }
+    });
+}
 
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+function createVisualization(data: Array<ImportedData>) {
+    // setup a scene
+    var scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xffffff);
+    var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
+    camera.position.set(463700, 2800, 6834000);
+    camera.lookAt(463559, 2393, 6833876);
 
-camera.position.z = 5;
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
 
-var animate = function () {
-    requestAnimationFrame(animate);
+    //create a blue LineBasicMaterial
+    var material = new THREE.LineBasicMaterial({ color: 0x0000ff });
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    
-    renderer.render(scene, camera);
-};
+    var geometry = new THREE.Geometry();
 
-animate();
+    data.forEach(point => {
+        geometry.vertices.push(new THREE.Vector3(point.X, point.DEM, point.Y));
+    });
+
+    var line = new THREE.Line(geometry, material);
+
+    scene.add(line);
+
+    // camera.position.z = 5;S
+
+    var animate = function () {
+        requestAnimationFrame(animate);
+
+        // line.rotation.x += 0.01;
+        // line.rotation.y += 0.01;
+
+        renderer.render(scene, camera);
+    };
+
+    animate();
+}
+
+interface ImportedData{
+    X: number,
+    Y: number,
+    DEM: number
+}
